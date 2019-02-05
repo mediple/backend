14de2dffbd4d98282ed7b89e46d5b4458333a237
noname_Producer <nonamep765@gmail.com>
2019-02-05 20:05:45 +0900

    x-www-form api

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 import json
+import time
 
 import responder
 from tortoise import Tortoise
@@ -13,28 +14,30 @@ async def close_db_connection(self):
     await Tortoise.close_connections()
 
 
-@api.route("/")
-async def home(req, resp):
+# x-www 방식
+@api.route("/api/register/x-www")
+async def home(req, res):
+    # await Tortoise.init(
+    #     db_url=config_secret['mediple']['database']['url'], modules={"models": ["mediple.models"]}
+    # )
+
+    @api.background.task
+    def progress_data(data):
+        print(data)
+        time.sleep(3)
+
+    data = await req.media()
+
+    progress_data(data)
+
+    res.media = {'success': True}
+
+
+@api.route("/api/login")
+async def home(req, res):
     await Tortoise.init(
         db_url=config_secret['mediple']['database']['url'], modules={"models": ["mediple.models"]}
     )
-
-    await User.create(name="Test User")
-    user = await User.first()
-
-    resp.text = f"Hello, {user.name}"
-
-
-@api.route("/id/{id}")
-async def home(req, resp, id):
-    await Tortoise.init(
-        db_url=config_secret['mediple']['database']['url'], modules={"models": ["mediple.models"]}
-    )
-
-    await User.create(name="Test User")
-    user = await User.first()
-
-    resp.text = f"Hello, {user.name}"
 
 
 api.run(address='0.0.0.0', port=6765, debug=True)
